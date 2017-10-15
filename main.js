649d09537a0a4998d022a35f1ca65ed1c63a448a
valeshan <valeshan@gmail.com>
2017-10-16 09:28:28 +1100

    base files
    initial files for site

---
+++ main.js
@@ -0,0 +1,11 @@
+//$('#searchbtn').click(function(){
+//  let searchItem = $("#search").text();
+//$.get("https://www.googleapis.com/customsearch/v1)",
+//        {
+//q: searchItem,
+//          imgSize: "medium",
+//searchType: "image",
+//          filetype: "jpg",
+//key: AIzaSyDIb_HYFzmL_V_pnkhQHxBUU5NMUiUVpHI,
+//          cx: 000204171608826949111:q2ewxpxco_a
+//        },
